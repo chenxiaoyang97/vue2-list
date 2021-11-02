7fe88ff0dc1f622eb8f33f873e7527bddf565f75
chenxiaoyang97 <93501387+chenxiaoyang97@users.noreply.github.com>
2021-11-02 15:48:36 +0800

    第一次提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,52 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import cart from '../cart_home.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'Home',
+    component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/cart_home',
+    name: 'cart',
+    component: cart
+  },
+  {
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    children:[
+     {
+      path: '',
+      name: 'xtsy',
+      component: () => import('../views/xtsy/index.vue'),
+     },
+     {
+      path: 'jbxx',
+      name: 'jbxx',
+      component: () => import('../views/jbxx/index.vue'),
+     },
+     {
+      path: 'kcgl',
+      name: 'kcgl',
+      component: () => import('../views/kcgl/index.vue'),
+     }
+    ]
+  }
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
